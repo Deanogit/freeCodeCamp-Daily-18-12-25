41e0933cbbcba0eabb830cfce70ac4384a463798
Dean Howe <hideanhowe@gmail.com>
2026-01-06 21:31:15 +0000

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+// Checkerboard
+// Given an array with two numbers, the first being the number of rows and the second being the number of columns, return a matrix (an array of arrays) filled with "X" and "O" characters of the given size.
+
+// The characters should alternate like a checkerboard.
+// The top-left cell must always be "X".
+// For example, given [3, 3], return:
+
+// [
+//   ["X", "O", "X"],
+//   ["O", "X", "O"],
+//   ["X", "O", "X"]
+// ]
